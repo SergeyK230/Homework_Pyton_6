48e79b7f1a7935f394801565537e30d75ef5f2db
sergey.k <k.sergey2010@yandex.ru>
2022-08-02 20:01:26 +0700

    zadacha_3 done

--- zadacha_3.py
+++ zadacha_3.py
@@ -1,13 +1,5 @@
 # Найти сумму чисел списка стоящих на нечетной позиции
 
 list = [1, 2, 3, 5, 1, 5, 3, 10]
-list = [list[i] for i in range(len(list)) if not i%2]
-sum = 0
-s = lambda s, x: s + x
-p = lambda l, x:
+list = sum([list[i] for i in range(len(list)) if not i%2])
 print(list)
-
-
-# l = lambda s,x: s + x
-# list = 
-# sum = 0
