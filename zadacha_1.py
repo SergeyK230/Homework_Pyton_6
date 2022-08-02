67a945fae60d201116832f8c1877a704e65670a7
sergey.k <k.sergey2010@yandex.ru>
2022-08-03 01:46:15 +0700

    zadacha_1 done

--- zadacha_1.py
+++ zadacha_1.py
@@ -1,28 +1,102 @@
 #  1) Доделать реализацию функции eval со скобками
 
-import re
-
-actions = {
-"^": lambda x, y: str(float(x) ** float(y)),
-"*": lambda x, y: str(float(x) * float(y)),
-"/": lambda x, y: str(float(x) / float(y)),
-"+": lambda x, y: str(float(x) + float(y)),
-"-": lambda x, y: str(float(x) - float(y))
-}
-
-priority_reg_exp = r"\((.+?)\)"
-action_reg_exp = r"(-?\d+(?:\.\d+)?)\s*\{}\s*(-?\d+(?:\.\d+)?)"
+from os import remove
 
 
-def my_eval(expresion: str) -> str:
-    while (match := re.search(priority_reg_exp, expresion)):
-        expresion: str = expresion.replace(match.group(0), my_eval(match.group(1)))
+def new_eval(text):
+    sibols = ['(', ')', '*', '/', '+', '-', '^']
+    result = []
+    mem = ''
+    for i in text:
+        if i not in sibols:
+            mem += i
+        elif i in sibols:
+            if mem != '':
+                result.append(float(mem))
+            result.append(i)
+            mem = ''
+    result.append(float(mem))
+    return(reshenie(result))
 
-    for symbol, action in actions.items():
-        while (match := re.search(action_reg_exp.format(symbol), expresion)):
-            expresion: str = expresion.replace(match.group(0), action(*match.groups()))
+def scobki(prim):
+    i = 0
+    j = -1
+    vrem = 0
+    while '(' in prim:
+        if prim[i] == '(':
+            while True:
+                if prim[j] == ')':
+                    vrem = prim[i+1:j]
+                    vrem = reshenie(vrem)
+                    prim[j] = vrem
+                    del prim[i:j]
+                    j = -1
+                    break
+                else:
+                    j -= 1
+        else:
+            i +=1
+    return prim
 
-    return expresion
+def reshenie(prim):
+    if '(' in prim:
+        prim = scobki(prim)
 
-exp = "".join(input('Введите строковое выражение: ').split())
-print(my_eval(exp), eval(exp))
+    i = 0
+    while '^' in prim:
+        if prim[i] ==  '^':
+            prim[i+1] = deistvie(prim[i-1], prim[i+1], prim[i])
+            del prim[i-1:i+1]
+            i = 0
+        else:
+            i += 1
+           
+    i = 0
+    while ('*' in prim) or ('/' in prim):
+        if prim[i] == '*' or prim[i] == '/':
+            prim[i+1] = deistvie(prim[i-1], prim[i+1], prim[i])
+            del prim[i-1:i+1]
+            i = 0
+        else:
+            i += 1
+    
+    i = 0
+    while ('+' in prim) or ('-' in prim):
+        if prim[i] == '+' or prim[i] == '-':
+            prim[i+1] = deistvie(prim[i-1], prim[i+1], prim[i])
+            del prim[i-1:i+1]
+            i = 0
+        else:
+            i += 1
+            
+    return prim[0]
+
+def deistvie(arg1,arg2,znak):
+    match znak:
+        case '^':
+            return arg1**arg2
+        case '*':
+            return arg1*arg2
+        case '/':
+            return arg1 / arg2
+        case '+':
+            return arg1 + arg2
+        case '-':
+            return arg1 - arg2
+
+
+primer = '1+2*3+4*5'
+print(eval(primer), new_eval(primer))
+primer = '(1+2)*3+4*5'
+print(eval(primer), new_eval(primer))
+primer = '((1+2)*3+4)*5'
+print(eval(primer), new_eval(primer))
+primer = '((1+2)*3+4)*5^2'
+print(eval('((1+2)*3+4)*5**2'), new_eval(primer))
+
+
+
+
+
+
+    
